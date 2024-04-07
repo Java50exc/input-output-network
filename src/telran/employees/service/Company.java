74dd68354ea30ea5fff9aa4be661bd976eb206ff
allanteone <allanteone@gmail.com>
2024-04-07 23:12:19 +0300

    fixes restore method implementation

--- src/telran/employees/service/Company.java
+++ src/telran/employees/service/Company.java
@@ -6,31 +6,34 @@ import telran.employees.dto.Employee;
 
 public interface Company {
 	boolean addEmployee(Employee empl);
+
 	Employee removeEmployee(long id);
+
 	Employee getEmployee(long id);
+
 	List<Employee> getEmployees();
-	
+
 	@SuppressWarnings("unchecked")
 	default void restore(String dataFile) {
 		List<Employee> employees = null;
-		
+
 		try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(dataFile))) {
 			employees = (List<Employee>) in.readObject();
+		} catch (FileNotFoundException e) {
 		} catch (IOException | ClassNotFoundException e) {
 			new RuntimeException(e.getMessage());
 		}
 		employees.forEach(this::addEmployee);
 	}
-	
+
 	default void save(String dataFile) {
 		List<Employee> employees = getEmployees();
-		
+
 		try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(dataFile))) {
 			out.writeObject(employees);
 		} catch (IOException e) {
 			throw new RuntimeException(e.getMessage());
 		}
 	}
-	
 
 }
