38fe293b93cf6828ed19f5b41216c1407235d3c0
allanteone <allanteone@gmail.com>
2024-04-07 17:38:20 +0300

    adds service tests

---
+++ src/telran/employees/test/CompanyTests.java
@@ -0,0 +1,35 @@
+package telran.employees.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class CompanyTests {
+	final static String TEST_FILE_NAME = "test.data";
+
+	@BeforeEach
+	void setUp() {
+
+	}
+
+	@Test
+	void testAddEmployee() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRemoveEmployee() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetEmployee() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetEmployees() {
+		fail("Not yet implemented");
+	}
+
+}
