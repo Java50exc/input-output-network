dc158473656f2df8f8ba1dcbd9239307bb762840
allanteone <allanteone@gmail.com>
2024-04-07 23:29:06 +0300

    updates service tests

--- src/telran/employees/test/CompanyTests.java
+++ src/telran/employees/test/CompanyTests.java
@@ -21,59 +21,58 @@ class CompanyTests {
 	@BeforeEach
 	void setUp() {
 		company = new CompanyImpl();
+		company.addEmployee(EMPLOYEE);
 	}
 
 	@Test
 	void addEmployee_correctFlow_success() {
-		company.addEmployee(EMPLOYEE);
 		assertTrue(company.getEmployees().size() == 1);
-		company.addEmployee(EMPLOYEE);
+	}
+	
+	@Test
+	void addEmployee_alreadyExists_returnsFalse() {
+		assertFalse(company.addEmployee(EMPLOYEE));
 		assertTrue(company.getEmployees().size() == 1);
 	}
 
 	@Test
 	void removeEmployee_correctFlow_success() {
-		company.addEmployee(EMPLOYEE);
 		assertTrue(company.getEmployees().size() == 1);
-		company.removeEmployee(EMPLOYEE.id());
+		assertEquals(EMPLOYEE, company.removeEmployee(EMPLOYEE.id()));
 		assertTrue(company.getEmployees().isEmpty());
 	}
 	
 	@Test
 	void removeEmployee_notExists_returnsNull() {
-		company.addEmployee(EMPLOYEE);
 		assertNull(company.removeEmployee(EMPLOYEE.id() + 1));
 		assertFalse(company.getEmployees().isEmpty());
 	}
 
 	@Test
 	void getEmployee_correctFlow_success() {
-		company.addEmployee(EMPLOYEE);
 		assertEquals(EMPLOYEE, company.getEmployee(EMPLOYEE.id()));
 	}
 	
 	@Test
 	void getEmployee_notExists_returnsNull() {
-		company.addEmployee(EMPLOYEE);
 		assertNull(company.getEmployee(EMPLOYEE.id() + 1));
 	}
 
 	@Test
 	void getEmployees_correctFlow_success() {
-		company.addEmployee(EMPLOYEE);
 		assertIterableEquals(List.of(EMPLOYEE), company.getEmployees());
 	}
 	
 	@Test
 	@Order(1)
 	void save_correctFlow_success() {
-		company.addEmployee(EMPLOYEE);
 		company.save(TEST_FILE_NAME);
 	}
 	
 	@Test
 	@Order(2)
 	void restore_correctFlow_success() {
+		company = new CompanyImpl();
 		company.restore(TEST_FILE_NAME);
 		company.getEmployees().forEach(e -> assertEquals(EMPLOYEE, e));
 	}
