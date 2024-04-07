8b4cc0d0f032da28be5aa09236c2a01246f7e586
allanteone <allanteone@gmail.com>
2024-04-07 23:03:47 +0300

    adds save and restore implementations

--- src/telran/employees/service/Company.java
+++ src/telran/employees/service/Company.java
@@ -1,6 +1,8 @@
 package telran.employees.service;
 
+import java.io.*;
 import java.util.List;
+import static telran.employees.api.ErrorMessages.*;
 import telran.employees.dto.Employee;
 
 public interface Company {
@@ -9,12 +11,26 @@ public interface Company {
 	Employee getEmployee(long id);
 	List<Employee> getEmployees();
 	
+	@SuppressWarnings("unchecked")
 	default void restore(String dataFile) {
+		List<Employee> employees = null;
 		
+		try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(dataFile))) {
+			employees = (List<Employee>) in.readObject();
+		} catch (IOException | ClassNotFoundException e) {
+			new RuntimeException(e.getMessage());
+		}
+		employees.forEach(this::addEmployee);
 	}
 	
 	default void save(String dataFile) {
+		List<Employee> employees = getEmployees();
 		
+		try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(dataFile))) {
+			out.writeObject(employees);
+		} catch (IOException e) {
+			throw new RuntimeException(SAVE_ERROR_MESSAGE);
+		}
 	}
 	
 
